35051a71594e22ef7c63f0ac62627dac3819dbe0
SngGrozy <guozhi.sun@yolu-inc.com>
2015-12-05 01:51:28 +0800

    添加代码注释

--- scope/scope/main.cpp
+++ scope/scope/main.cpp
@@ -8,7 +8,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
     std::cout << "Hello, World!\n";
     int sum = 0;
@@ -19,3 +20,7 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
+/*
+ 这段程序定义了3个名字:main、sum和val，同时使用了命名空间名字std，该空间提供了2个名字cout和cin供程序使用。
+ 名字main定义于所有花括号以外。它和其他大多数定义在函数体之外的名字一样有全局作用域(global scope)。一旦声明之后，全局作用域内的名字在整个程序的范围之内都可以使用。 sum定义在main函数所限定的作用域之内，从生命sum开始知道main函数结束位置都可以访问它，但是除了main函数的区域便不可访问。因此说变量sum拥有块作用域(block scope)。名字val定义域fo语句内，在for语句之内可以访问val，但是在main函数的其他部分便不可访问它了。
+*/
