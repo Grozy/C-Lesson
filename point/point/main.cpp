c2f266a008ad47b90da837791f02773454b1a8dc
SngGrozy <guozhi.sun@yolu-inc.com>
2015-12-05 13:44:55 +0800

    指针

---
+++ point/point/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  point
+//
+//  Created by 孙国志 on 15/12/5.
+//  Copyright (c) 2015年 grozy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
